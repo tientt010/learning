53e34df811dc1bd6fe8528cbb237f613b6e268dd
tieu cot <tieucottt010@gmail.com>
2023-04-21 22:47:41 +0700

    test

--- set.cpp
+++ set.cpp
@@ -1,80 +1,43 @@
-#include<bits/stdc++.h>
+#include<iostream>
 #include<set>
 
 using namespace std;
 // set sẽ không lưu các phần tử trùng nhau
 // set sẽ tự động sắp xếp các phần tử theo thứ tự từ nhỏ đến lớn 
+struct cmp{														//viết lại hàm so sánh trong set(từ lớn tới bé)
+	bool operator()(int a,int b){								//biến so sánh
+		return a>b;												//nếu a>b return 1 ngược lại return 0
+	}
+};
 int main(){
-	set<int> s;
-	s.insert(100);										//thêm 1 phần tử
-	s.insert(700);
-	s.insert(300);
-	s.insert(400);
-	s.insert(100);
-	s.insert(300);
-	cout<<s.size()<<endl;								//in ra số phần tử
-	if(s.find(700)!=s.end())							//kiểm tra xem 700 có nằm trong set hay không.
-	{													//bản chất của s.find() là tìm đến interester(la lá con trỏ) có chứa phần tử 700 
-		cout<<"700 co trong set"<<endl;
+	set<int> a;
+	a.insert(4);												//dùng hàm a.insert(x) để thêm x vào a		O(log(n))
+	a.insert(3);
+	a.insert(7);
+	a.insert(6);
+	a.insert(4);												//không thể thêm 1 phần tử nếu phần tử đó đã tồn tại trong set
+	for(set<int>::iterator it=a.begin();it!=a.end();it++){		//cũng như các container khác có thể sử dụng iterator để duyệt set
+		cout<<*it<<" ";
+	}
+	cout<<endl;
+	cout<<a.size()<<endl;										//hàm size() dùng để truy xuất đến số phần tử của set		O(1)
+	if(!a.empty())cout<<"set a khong rong"<<endl;				//hàm empty() trả về true(1) nếu set rỗng ngược lại trả về false(0)		O(1)
+	set<int,cmp> b;												//có thể khai báo set bằng cách set<int,cmp> với cmp là 1 struct
+	b.insert(4);												//bây giờ set b sẽ có quy luật sắp xếp được viết trong struct cmp(từ lớn tới bé)
+	b.insert(3);
+	b.insert(7);
+	b.insert(6);
+	b.insert(4);
+	for(int x:b){												//có thể duyệt set bằng vòng lặp for x
+		cout<<x<<" ";
+	}
+	cout<<endl;
+	set<int>::iterator it=b.find(4);							//hàm find(x)sẽ trả về iterator trỏ đến phần tử mang giá trị x trong set
+	if(it==b.end()){											//trong trường hợp không tồn tại x trong set hàm sẽ trả về s.end()
+		cout<<"khong ton tai 4 trong b"<<endl;
 	}else{
-		cout<<"khong tim thay 700"<<endl;
+		cout<<"ton tai 4 trong b"<<endl;
 	}
-	if(s.count(400)!=0)									//kiểm tra xem 400 có nằm trong set hay không tương tự như find.
-	{													//bản chất cuẩ count là đếm xem có bao nhiêu phần tử trong set. 
-		cout<<"400 co trong set"<<endl;
-	}else{
-		cout<<"khong tim thay 400"<<endl;
-	}
-	for(int x : s)										//in ra màn hình các phần tử của set
-	{
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	for(auto x:s){
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	s.erase(700);										//xóa 1 phần tử trong set
-	for(int x:s){
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	cout<<*s.begin()<<endl;								//xuất ra phần tử đầu tiên có trong set(phần tử nhỏ nhất)
-	cout<<*s.rbegin()<<endl;							//chỏ đến phần tử đối diện phần tử đầu tiên(phần tử cuối cùng)
-	cout<<*s.lower_bound(300)<<endl;					//trả về iterator trỏ tới phần tử nhỏ nhất có giá trị lớn hơn hoặc bằng khóa key trong set.
-	cout<<*s.upper_bound(300)<<endl;					//trả về iterator trỏ tới phần tử nhỏ nhất có giá trị lớn hơn khóa key trong set.
-	set<int> b;
-	int n;												//có thể coi set như 1 hàm sắp xếp nhanh
-	cin>>n;
-	for(int i=0; i<n;i++){
-		int x;
-		cin >>x;
-		b.insert(x);
-	}
-	for(int x:b){
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	multiset<string> ms;								//multiset giống y hệt set dùng chung các hàm của set nhưng chỉ khác là nó chấp nhận nhiều phần tử giống nhay
-	ms.insert("tien");
-	ms.insert("tuyen");
-	ms.insert("thinh");
-	ms.insert("thai");
-	ms.insert("tu");
-	ms.insert("thang");
-	ms.insert("tien");
-	ms.insert("tu");
-	ms.insert("tuan");
-	cout<<ms.size()<<endl;
-	for(string x:ms){
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	ms.erase("tien");									// khi sử dụng hàm erase trong multiset thì nó sẽ xóa tất cả các phần tử được khai báo.
-	for(string x:ms){
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	return 0;	
+	return 0;		
 }
 
